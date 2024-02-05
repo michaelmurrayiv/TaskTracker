9f3fabb1c3662191c2bb3c159344ad65a77ab432
Michael Murray <michael.murray.iv@gmail.com>
2024-02-04 23:13:12 -0800

    updated css and styling to reflect Figma design scheme

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
-const sampleTasks = [
-  "finish website", 
-  "work out",
-  "apply for job"
-]
-
 function Tasks(props) {
   return (
     <div className="task-container">
       {props.title}
       <ul className="task-list">
-        {sampleTasks.map((task, index) => (
+        {props.data.map((task, index) => (
           <li className="tasks" key={index}>{task}</li>
         ))}
       </ul>
