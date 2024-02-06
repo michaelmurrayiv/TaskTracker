431a6ad67519cccf285db56e8e4a2c85b1473199
Michael Murray <michael.murray.iv@gmail.com>
2024-02-05 22:25:21 -0800

    changed backend port and setup cors within express. Data fetched from the backend now visible in frontend.?

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 9999;
 const tasks = require('./dummy_data');
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/tasks", (req, res) => {
