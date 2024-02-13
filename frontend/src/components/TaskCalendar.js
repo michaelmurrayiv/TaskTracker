7b54a770257a5a4b1246e6dd9779ad658d4a5f64
Michael Murray <michael.murray.iv@gmail.com>
2024-02-13 10:26:19 -0800

    improved css styling for calendar : :

--- frontend/src/components/TaskCalendar.js
+++ frontend/src/components/TaskCalendar.js
@@ -4,6 +4,7 @@ import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./TaskCalendar.css";
 
 const localizer = momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
@@ -16,8 +17,6 @@ function TaskCalendar() {
 			title: "Some title",
 		},
 	]);
-
-
 
 	const onEventDrop = (data) => {
 		console.log(data);
